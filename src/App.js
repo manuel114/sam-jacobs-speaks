583519e968089f4e7594c7ae0734cc842b5a3160
NoraZheng <zhengnuo@gmail.com>
2019-09-12 02:18:13 -0400

    tested css crid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import firebase from './firebase';
 import './App.css';
 
-function App() {
-	return (
-		<div className='App'>
-			<h1>Zoltar Speaks</h1>
-		</div>
-	);
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      maze: [
+        [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+        [1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1],
+        [1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1],
+        [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
+        [1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1]
+      ]
+    };
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Zoltar Speaks</h1>
+        <div className='maze'>
+          {this.state.maze.map((item, index) => {
+            return item.map((cell, i) => {
+              if (cell === 0) {
+                return <div className='cell path'>path</div>;
+              } else {
+                return <div className='cell wall'>wall</div>;
+              }
+            });
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
