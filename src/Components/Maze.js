c480ad829a3481c319a743065708c30f65160815
Nora Zheng <zhengnuo@gmail.com>
2019-09-13 15:15:58 -0400

    created a layer for coin to move

--- src/Components/Maze.js
+++ src/Components/Maze.js
@@ -1,46 +1,59 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Maze extends Component {
-constructor(props){
-   super(props)
-   this.state={
-      mazeMap: [
-                 [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-                 [1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-                 [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1],
-                 [1, 1, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1],
-                 [1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1],
-                 [1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1],
-                 [1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
-                 [1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-                 [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1],
-                 [1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
-                 [1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
-                 [1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1]
-               ]
-             }
-   }
+	constructor(props) {
+		super(props);
+		this.state = {
+			mazeMap: [
+				[-1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+				[0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+				[1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1],
+				[1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1],
+				[1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+				[1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+				[1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+				[1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+				[1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+				[1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
+				[1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
+				[1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0]
+			],
+			playerLocation: {
+				x: 0,
+				y: 0
+			}
+		};
+	}
 
-    render() {
-
-        return (
-          <div>
-            <h1>Maze</h1>
-            <div className='maze'>
-           {this.state.mazeMap.map((item, index) => {
-              return item.map((cell, i) => {
-                if (cell === 0) {
-                  return <div className='cell path'>path</div>;
-                } else {
-                  return <div className='cell wall'>wall</div>;
-                }
-              });
-            })}
-          </div>
-          </div>
-        );
-    }
-
+	render() {
+		return (
+			<div>
+				<h1>Maze</h1>
+				<div className='mazeLayer'>
+					{this.state.mazeMap.map((row, Y) => {
+						return row.map((cell, X) => {
+							if (cell !== 1) {
+								return <div className='mapCell path'>path</div>;
+							} else {
+								return <div className='mapCell wall'>wall</div>;
+							}
+						});
+					})}
+					<div className='movingLayer'>
+						{this.state.mazeMap.map((row, Y) => {
+							return row.map((cell, X) => {
+								if (cell === -1) {
+									return <div className='movingCell player'>Player</div>;
+								} else {
+									return <div className='movingCell'>O</div>;
+								}
+							});
+						})}
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Maze;
