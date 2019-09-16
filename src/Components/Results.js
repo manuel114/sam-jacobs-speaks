9ec1b360096f1982ae66e184b599b0c8002d531a
Konrad Droeske <konrad@ottosdoner.com>
2019-09-16 12:15:21 -0400

    added styling, resetting the master

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 const Results = props => {
-	return (
-		<div>
-			<p>{props.finalAnswer}</p>
-		</div>
-	);
+  return (
+    <div className="wrapper">
+      <main>
+        <div className="zoltarCard">
+          <h2 className="zoltarGranted">your wish is granted </h2>
+          <p className="finalAnswer">{props.finalAnswer}</p>
+        </div>
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+      </main>
+    </div>
+  );
 };
-
 export default Results;
--- src/Components/WinModal.js
+++ src/Components/WinModal.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
 const WinModal = props => {
   return (
     <div>
-      <p>Your wish has been granted!</p>
-
       <Link to="/results">
         <button>Unveil Your Wish</button>
       </Link>
     </div>
   );
 };
-
 export default WinModal;
