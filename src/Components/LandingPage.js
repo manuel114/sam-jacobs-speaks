9c1eeac1a468e51da9b2ed8369fa7d3302566602
Konrad Droeske <konrad@ottosdoner.com>
2019-09-17 17:21:27 -0400

    landing page styling

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,79 +1,75 @@
-import React, { Component } from 'react';
-
-import { Link } from 'react-router-dom';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import coinSlot from "../Assets/coinSlot.svg";
 class LandingPage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			userWish: '',
-			wishEmpty: false
-		};
-	}
-
-	handleChange = e => {
-		let userInput = e.target.value;
-
-		this.setState({ userWish: userInput });
-	};
-
-	emptyStringCheck = (event, userWish) => {
-		if (userWish !== '') {
-			this.props.handleSubmit(userWish);
-		} else {
-			event.preventDefault();
-			this.setState(
-				{
-					wishEmpty: true
-				},
-				() => {
-					console.log(this.state.wishEmpty);
-				}
-			);
-		}
-	};
-
-	render() {
-		return (
-			<main className='wrapper zoltarContainer'>
-				<h1>Zoltar Speaks</h1>
-
-				<img
-					className='zoltarImage'
-					src={require('../Assets/ZoltarLogo.svg')}
-					alt={'Zoltar Speaks Logo'}
-				/>
-
-				<form className='makeWishContainer' action='submit'>
-					<h2 className='wishQuestion'>What do you wish for?</h2>
-
-					<div className='buttonLinkContainer'>
-						<label className='wishLabel visuallyHidden'>Enter a wish</label>
-
-						<input
-							required
-							type='text'
-							placeholder='enter what you desire here'
-							onChange={this.handleChange}
-							className={this.state.wishEmpty === true ? 'wishError' : ''}
-						/>
-
-						<div className='wishButton'>
-							<Link to='/maze'>
-								<button
-									className='landingPageButton'
-									onClick={event =>
-										this.emptyStringCheck(event, this.state.userWish)
-									}>
-									Make wish
-								</button>
-							</Link>
-						</div>
-					</div>
-				</form>
-			</main>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      userWish: "",
+      wishEmpty: false
+    };
+  }
+  handleChange = e => {
+    let userInput = e.target.value;
+    this.setState({ userWish: userInput });
+  };
+  emptyStringCheck = (event, userWish) => {
+    if (userWish !== "") {
+      this.props.handleSubmit(userWish);
+      this.setState({
+        wishEmpty: false
+      });
+    } else {
+      event.preventDefault();
+      this.setState(
+        {
+          wishEmpty: true
+        },
+        () => {
+          console.log(this.state.wishEmpty);
+        }
+      );
+    }
+  };
+  render() {
+    return (
+      <main className="wrapper zoltarContainer">
+        <h1>Zoltar Speaks</h1>
+        <img
+          className="zoltarImage"
+          src={require("../Assets/ZoltarLogo.svg")}
+          alt={"Zoltar Speaks Logo"}
+        />
+        <form className="makeWishContainer" action="submit">
+          <div className="leftForm">
+            <h2 className="wishQuestion">What do you wish for?</h2>
+            <label className="wishLabel visuallyHidden">Enter a wish</label>
+            <input
+              // required
+              type="text"
+              placeholder="Enter A Wish & Insert Coin"
+              onChange={this.handleChange}
+              className={`wishInputBox ${
+                this.state.wishEmpty === true ? "wishError" : ""
+              }`}
+            />
+          </div>
+          <div className="buttonLinkContainer">
+            <Link to="/maze">
+              <img
+                type="button"
+                src={coinSlot}
+                alt="Coin Slot"
+                className="landingPageButton"
+                onClick={event =>
+                  this.emptyStringCheck(event, this.state.userWish)
+                }
+              />
+            </Link>
+          </div>
+        </form>
+      </main>
+    );
+  }
 }
-
 export default LandingPage;
