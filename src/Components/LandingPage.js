c532fbe971a5d97f6d091e28ac4f37752342d482
Konrad Droeske <konrad@ottosdoner.com>
2019-09-16 21:22:07 -0400

    got rid of animation jankyness on landing page, moved state to the actual componenent to prevent render

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -7,9 +7,12 @@ class LandingPage extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			userWish: ''
+      userWish: '',
+      wishEmpty: false
+
 		};
-	}
+  }
+  
 handleChange = e => {
     let userInput = e.target.value;
 
@@ -17,46 +20,62 @@ handleChange = e => {
       { userWish: userInput })
 
   };
+
+
+  emptyStringCheck = (event, userWish) => {
+    
+    if (userWish !== "") {
+      this.props.handleSubmit(userWish);
+    } else {
+
+      event.preventDefault();
+      this.setState({
+        wishEmpty: true,
+      }, () => {
+        console.log(this.state.wishEmpty);
+      })
+    }
+  }
+
 	render() {
 		return (
-			<main className='wrapper zoltarContainer'>
-				<h1>Zoltar Speaks</h1>
+      <main className="wrapper zoltarContainer">
+        <h1>Zoltar Speaks</h1>
 
-				<img
-					className='zoltarImage'
-					src={require('../Assets/ZoltarLogo.svg')}
-					alt={'Zoltar Speaks Logo'}
-				/>
+        <img
+          className="zoltarImage"
+          src={require("../Assets/ZoltarLogo.svg")}
+          alt={"Zoltar Speaks Logo"}
+        />
 
-				<form className='makeWishContainer' action='submit'>
-					<h2 className='wishQuestion'>What do you wish for?</h2>
+        <form className="makeWishContainer" action="submit">
+          <h2 className="wishQuestion">What do you wish for?</h2>
 
-					<div className='buttonLinkContainer'>
-						<label className='wishLabel visuallyHidden'>Enter a wish</label>
+          <div className="buttonLinkContainer">
+            <label className="wishLabel visuallyHidden">Enter a wish</label>
 
-						<input
-							required
-							type='text'
-							placeholder='enter what you desire here'
-							onChange={this.handleChange}
-							className={this.props.wishEmpty === true ? 'wishError' : ''}
-						/>
+            <input
+              required
+              type="text"
+              placeholder="enter what you desire here"
+              onChange={this.handleChange}
+              className={this.state.wishEmpty === true ? "wishError" : ""}
+            />
 
-						<div className='wishButton'>
-							<Link to='/maze'>
-								<button
-									className='landingPageButton'
-									onClick={event =>
-										this.props.handleSubmit(event, this.state.userWish)
-									}>
-									Make wish
-								</button>
-							</Link>
-						</div>
-					</div>
-				</form>
-			</main>
-		);
+            <div className="wishButton">
+              <Link to="/maze">
+                <button
+                  className="landingPageButton"
+                  onClick={(event) => this.emptyStringCheck(event, this.state.userWish)}
+                >
+                  Make wish
+                </button>
+              </Link>
+            </div>
+          </div>
+        </form>
+      </main>
+    );
 	}
 
  
