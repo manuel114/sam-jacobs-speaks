ae838221f36182249f24f6fb6422d3e69096e469
NoraZheng <zhengnuo@gmail.com>
2019-09-24 01:40:22 -0400

    Did Axe Accessibility testing

--- src/App.js
+++ src/App.js
@@ -5,98 +5,106 @@ import './Partials/App.scss';
 import Maze from './Components/Maze';
 import LandingPage from './Components/LandingPage';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// ~~~ for AXE accessibility testing only ~~~
+
+// var ReactDOM = require('react-dom');
+// if (process.env.NODE_ENV !== 'production') {
+//   var axe = require('react-axe');
+//   axe(React, ReactDOM, 1000);
+// }
+// ~~~ for AXE accessibility testing only ~~~
 
 class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			advice: ''
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      advice: ''
+    };
+  }
 
-	handleSubmit = userWish => {
-		const userInput = userWish;
-		///run the imported filterWish function to remove filler words
-		const filteredWish = filterWish(userInput);
+  handleSubmit = userWish => {
+    const userInput = userWish;
+    ///run the imported filterWish function to remove filler words
+    const filteredWish = filterWish(userInput);
 
-		// turn the words into an array
-		const wordArray = filteredWish.split(' ');
+    // turn the words into an array
+    const wordArray = filteredWish.split(' ');
 
-		// take the first word of the array and save it
-		const userKeyWord = wordArray[0];
+    // take the first word of the array and save it
+    const userKeyWord = wordArray[0];
 
-		// run it through the API Call to get contextual advice
-		axios({
-			url: `https://api.adviceslip.com/advice/search/${userKeyWord}`,
-			method: `GET`,
-			dataResponse: `json`
-		}).then(answer => {
-			if (typeof answer.data.message === 'undefined') {
-				this.setState({
-					advice: answer.data.slips[0].advice
-				});
-			}
-		});
-	};
+    // run it through the API Call to get contextual advice
+    axios({
+      url: `https://api.adviceslip.com/advice/search/${userKeyWord}`,
+      method: `GET`,
+      dataResponse: `json`
+    }).then(answer => {
+      if (typeof answer.data.message === 'undefined') {
+        this.setState({
+          advice: answer.data.slips[0].advice
+        });
+      }
+    });
+  };
 
-	componentDidMount() {
-		// on mount, make an API call, get a random piece of advice and store in state, just in case the user's keyword query doesn't return any result
-		this.getRandomQuote();
-	}
-	getRandomQuote = () => {
-		axios({
-			url: `https://api.adviceslip.com/advice`,
-			method: `GET`,
-			dataResponse: `json`
-		})
-			.then(answer => {
-				const randomAdvice = answer.data.slip.advice;
-				this.setState({
-					advice: randomAdvice
-				});
-			})
-			.catch(() => {
-				// if ajax call for random advice failed (API down) return this error message
-				this.setState({
-					advice: `Zoltar is resting and can't offer advice. Try again later.`
-				});
-			});
-	};
+  componentDidMount() {
+    // on mount, make an API call, get a random piece of advice and store in state, just in case the user's keyword query doesn't return any result
+    this.getRandomQuote();
+  }
+  getRandomQuote = () => {
+    axios({
+      url: `https://api.adviceslip.com/advice`,
+      method: `GET`,
+      dataResponse: `json`
+    })
+      .then(answer => {
+        const randomAdvice = answer.data.slip.advice;
+        this.setState({
+          advice: randomAdvice
+        });
+      })
+      .catch(() => {
+        // if ajax call for random advice failed (API down) return this error message
+        this.setState({
+          advice: `Zoltar is resting and can't offer advice. Try again later.`
+        });
+      });
+  };
 
-	render() {
-		return (
-			<Router>
-				<div className='app'>
-					<Route
-						exact
-						path='/'
-						component={() => (
-							<LandingPage
-								handleSubmit={this.handleSubmit}
-								wishEmpty={this.state.wishEmpty}
-							/>
-						)}
-					/>
+  render() {
+    return (
+      <Router>
+        <div className='app'>
+          <Route
+            exact
+            path='/'
+            component={() => (
+              <LandingPage
+                handleSubmit={this.handleSubmit}
+                wishEmpty={this.state.wishEmpty}
+              />
+            )}
+          />
 
-					<Route
-						path='/maze'
-						component={() => (
-							<Maze
-								finalAnswer={this.state.advice}
-								getRandomQuote={this.getRandomQuote}
-							/>
-						)}
-					/>
+          <Route
+            path='/maze'
+            component={() => (
+              <Maze
+                finalAnswer={this.state.advice}
+                getRandomQuote={this.getRandomQuote}
+              />
+            )}
+          />
 
-					<div className='starsContent'>
-						<div className='stars'></div>
-						<div className='stars2'></div>
-						<div className='stars3'></div>
-					</div>
-				</div>
-			</Router>
-		);
-	}
+          <div className='starsContent'>
+            <div className='stars'></div>
+            <div className='stars2'></div>
+            <div className='stars3'></div>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -3,95 +3,96 @@ import { Link } from 'react-router-dom';
 import coinSlot from '../Assets/coinSlot.svg';
 import coinSlotHover from '../Assets/coinSlotHover.svg';
 class LandingPage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			userWish: '',
-			wishEmpty: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      userWish: '',
+      wishEmpty: false
+    };
+  }
 
-	handleChange = e => {
-		let userInput = e.target.value;
-		this.setState({ userWish: userInput });
-	};
+  handleChange = e => {
+    let userInput = e.target.value;
+    this.setState({ userWish: userInput });
+  };
 
-	emptyStringCheck = (event, userWish) => {
-		// check if user input is empty, if not, then pass to user submit and reset wishEmpty state
-		if (userWish !== '') {
-			this.props.handleSubmit(userWish);
+  emptyStringCheck = (event, userWish) => {
+    // check if user input is empty, if not, then pass to user submit and reset wishEmpty state
+    if (userWish !== '') {
+      this.props.handleSubmit(userWish);
 
-			this.setState({
-				wishEmpty: false
-			});
-		} else {
-			// if user input is empty, prevent default, and toggle wishEmpty to add the error class to user
-			event.preventDefault();
-			this.setState(
-				{
-					wishEmpty: true
-				},
-				() => {
-					console.log(this.state.wishEmpty);
-				}
-			);
-		}
-	};
+      this.setState({
+        wishEmpty: false
+      });
+    } else {
+      // if user input is empty, prevent default, and toggle wishEmpty to add the error class to user
+      event.preventDefault();
+      this.setState(
+        {
+          wishEmpty: true
+        },
+        () => {
+          console.log(this.state.wishEmpty);
+        }
+      );
+    }
+  };
 
-	render() {
-		return (
-			<main className='wrapper zoltarContainer'>
-				<h1>Zoltar Speaks</h1>
-				<img
-					className='zoltarImage'
-					src={require('../Assets/ZoltarLogo.svg')}
-					alt={'Zoltar Speaks Logo'}
-				/>
+  render() {
+    return (
+      <main className='wrapper zoltarContainer'>
+        <h1>Zoltar Speaks</h1>
+        <img
+          className='zoltarImage'
+          src={require('../Assets/ZoltarLogo.svg')}
+          alt={'Zoltar Speaks Logo'}
+        />
 
-				<form
-					className='makeWishContainer'
-					action='submit'
-					onSubmit={event => event.preventDefault()}>
-					<div className='leftForm'>
-						<label className='wishLabel visuallyHidden'>
-							Enter a wish to start
-						</label>
-						<input
-							type='text'
-							placeholder='Enter A Wish & Insert Coin'
-							onChange={this.handleChange}
-							className={`wishInputBox ${
-								this.state.wishEmpty === true ? 'wishError' : ''
-							}`}
-						/>
-					</div>
-					<div className='buttonLinkContainer'>
-						<Link to='/maze'>
-							<div className='submitCard'>
-								<img
-									type='button'
-									src={coinSlot}
-									alt='Coin Slot'
-									className='landingPageButton'
-									onClick={event =>
-										this.emptyStringCheck(event, this.state.userWish)
-									}
-								/>
-								<img
-									type='button'
-									src={coinSlotHover}
-									alt='Coin Slot'
-									className='landingPageButtonHover'
-									onClick={event =>
-										this.emptyStringCheck(event, this.state.userWish)
-									}
-								/>
-							</div>
-						</Link>
-					</div>
-				</form>
-			</main>
-		);
-	}
+        <form
+          className='makeWishContainer'
+          action='submit'
+          onSubmit={event => event.preventDefault()}>
+          <div className='leftForm'>
+            <label htmlFor='wish' className='wishLabel visuallyHidden'>
+              Enter a wish to start
+            </label>
+            <input
+              id='wish'
+              type='text'
+              placeholder='Enter A Wish & Insert Coin'
+              onChange={this.handleChange}
+              className={`wishInputBox ${
+                this.state.wishEmpty === true ? 'wishError' : ''
+              }`}
+            />
+          </div>
+          <div className='buttonLinkContainer'>
+            <Link to='/maze'>
+              <div className='submitCard'>
+                <img
+                  type='button'
+                  src={coinSlot}
+                  alt='Coin Slot'
+                  className='landingPageButton'
+                  onClick={event =>
+                    this.emptyStringCheck(event, this.state.userWish)
+                  }
+                />
+                <img
+                  type='button'
+                  src={coinSlotHover}
+                  alt='Coin Slot'
+                  className='landingPageButtonHover'
+                  onClick={event =>
+                    this.emptyStringCheck(event, this.state.userWish)
+                  }
+                />
+              </div>
+            </Link>
+          </div>
+        </form>
+      </main>
+    );
+  }
 }
 export default LandingPage;
